b561e96e9af6b1172d634171a9d8fe9bd7f406bd
Bobby Westman <bobby.westman@hotmail.com>
2018-03-03 18:29:55 -0600

    background resizing

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 /**********************************************************/
 /*TODO*/
-/**********************************************************//*
+/**********************************************************/
 
+/*
 - stop overscrolling?
 - pin title bar to top and fade in nav bar
+- fade in tiles
+*/
 
-*//**********************************************************/
+/**********************************************************/
+/*notes*/
+/**********************************************************/
+
+/*
+- DONT use callback from scrolling: https://johnresig.com/blog/learning-from-twitter/
+*/
+
+/**********************************************************/
 
 //scroll to element on click
 /*$(document).ready(function (){
@@ -24,3 +35,27 @@ $(window).on('load', function () {
 var fadeInTitle = function() {
 	$("#title").addClass("fade-in-title");
 };
+
+//check scroll position
+// var outerPane = $details.find(".details-pane-outer");
+    
+// var didScroll = false;
+
+// $(window).scroll(function() {
+//     didScroll = true;
+// });
+ 
+// setInterval(function() {
+//     if ( didScroll ) {
+//         didScroll = false;
+//         // Check your page position and then
+//         // Load in more results
+//     }
+// }, 250);
+
+// $(function(){
+// 	var $w = $(window),
+// 		$background = $('#background');
+
+// 	$background.css
+// });
